dc63915cc229760a1cf230880a2fd88e023c9ab9
STmihan <mih.dunaev@gmail.com>
2022-03-24 22:53:02 +0200

    commit fix

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,8 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 80
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-project/'
+    : '/'
 })
